d659c8d8770795a874feabd5ec85855bbb9b1a0d
seducpma <naor@seducpma.com>
2016-08-25 16:25:19 -0300

    25-8-16

--- app/models/crianca.rb
+++ app/models/crianca.rb
@@ -10,40 +10,40 @@ class Crianca < ActiveRecord::Base
   validates_presence_of :nascimento
   validates_presence_of :opcao1
   before_save  :maiusculo
-  before_update  :reclassificacao
+  #before_update  :reclassificacao
   
   Status = %w(NA_DEMANDA CANCELADA MATRICULADA)
 
 
-  def reclassificacao
-    data=   (self.nascimento).to_s
-    hoje = Date.today.to_s
-    final = '2012-07-01'
-    if (hoje > data)  and (data >= final)
-      if  (data <= Date.today.to_s and data >= '2015-07-01')
-           self.grupo_id = 1
-      else if(data <= '2015-06-30' and data >= '2014-07-01')
-           self.grupo_id = 2
-           else if(data <= '2014-06-30' and data >= '2014-01-01')
-                  self.grupo_id = 4
-                else if (data <= '2013-12-31' and data >= '2013-07-01')
-                        self.grupo_id = 8
-                      else if (data <= '2013-06-30' and data >= '2012-07-01')
-                              self.grupo_id = 5
-                            else if (data <= '2012-06-30' and data >= '2011-07-01')
-                                  self.grupo_id = 6
-                                      else if(data <= '2011-06-30'and data >= '2010-07-01')
-                                            self.grupo_id = 7
-                                          end
-                                  end
-                            end
-                      end
-                end
-           end
-       end
- end
+  #def reclassificacao
+  #  data=   (self.nascimento).to_s
+  #  hoje = Date.today.to_s
+  #  final = '2012-07-01'
+  #  if (hoje > data)  and (data >= final)
+  #    if  (data <= Date.today.to_s and data >= '2015-07-01')
+  #         self.grupo_id = 1
+  #    else if(data <= '2015-06-30' and data >= '2014-07-01')
+  #         self.grupo_id = 2
+  #         else if(data <= '2014-06-30' and data >= '2014-01-01')
+  #                self.grupo_id = 4
+  #              else if (data <= '2013-12-31' and data >= '2013-07-01')
+  #                      self.grupo_id = 8
+  #                    else if (data <= '2013-06-30' and data >= '2012-07-01')
+  #                            self.grupo_id = 5
+  #                          else if (data <= '2012-06-30' and data >= '2011-07-01')
+  #                                self.grupo_id = 6
+  #                                    else if(data <= '2011-06-30'and data >= '2010-07-01')
+  #                                          self.grupo_id = 7
+  #                                        end
+  #                                end
+  #                          end
+  #                    end
+  #              end
+  #         end
+   #    end
+ #end
   
-  end
+#  end
 
 def self.na_demanda
     Crianca.find(:all, :conditions => {:status => 'NA_DEMANDA' })
@@ -66,19 +66,22 @@ def self.na_demanda
  def opcao
     data=self.nascimento
 
-  if  (self.nascimento <= '2015-12-31'.to_date and self.nascimento >= '2015-02-01'.to_date)
+  if (self.nascimento <= '2016-07-01'.to_date and self.nascimento >= '2015-06-01'.to_date)
        self.grupo_id = 1
-  else if(self.nascimento <= '2015-01-31'.to_date and self.nascimento >= '2014-07-01'.to_date)
-          self.grupo_id = 2
-       else if(self.nascimento <= '2014-06-30'.to_date and self.nascimento >= '2013-07-01'.to_date)
-              self.grupo_id = 4
-            else if(self.nascimento <= '2013-06-30'.to_date and self.nascimento >= '2012-07-01'.to_date)
-                    self.grupo_id = 5
-                  else if(self.nascimento <= '2012-06-30'.to_date and self.nascimento >= '2011-07-01'.to_date)
-                          self.grupo_id = 6
-                        else if(self.nascimento <= '2011-06-30'.to_date and self.nascimento >= '2010-07-01'.to_date)
-                               self.grupo_id = 7
-                             end
+  else if(self.nascimento <= '2015-06-30'.to_date and self.nascimento >= '2014-07-01'.to_date)
+                self.grupo_id = 2
+       else if (self.nascimento <= '2014-06-30'.to_date and self.nascimento >= '2014-01-01'.to_date)
+                    self.grupo_id = 4
+            else if(self.nascimento <= '2013-12-31'.to_date and self.nascimento >= '2013-07-01'.to_date)
+                          self.grupo_id = 8
+                  else if(self.nascimento <= '2013-06-30'.to_date and self.nascimento >= '2012-07-01'.to_date)
+                              self.grupo_id = 5
+                        else if(self.nascimento <= '2012-06-30'.to_date and self.nascimento >= '2011-07-01'.to_date)
+                                   self.grupo_id = 6
+                                 else if(self.nascimento <= '2011-06-30'.to_date and self.nascimento >= '2010-07-01'.to_date)
+                                          self.grupo_id = 7
+                                     end
+                                end
                        end
                  end
             end
@@ -88,31 +91,31 @@ def self.na_demanda
  
  end
 
- def self.alteracao_classe
-  @criancas_alteracao = Crianca.find( :all,:conditions => ["status = 'NA_DEMANDA'"])
- for crianca in @criancas_alteracao
-  if  (crianca.nascimento <= '2015-10-31'.to_date and crianca.nascimento >= '2015-02-01'.to_date)
-      crianca.grupo_id  = 11
-     else if(crianca.nascimento <= '2015-01-31'.to_date and crianca.nascimento >= '2014-07-01'.to_date)
-           crianca.grupo_id = 22
-         else if(crianca.nascimento <= '2014-06-30'.to_date and crianca.nascimento >= '2013-07-01'.to_date)
-               crianca.grupo_id = 44
-             else if(crianca.nascimento <= '2013-06-30'.to_date and crianca.nascimento >= '2012-07-01'.to_date)
-                    crianca.grupo_id = 54
-                  else if(crianca.nascimento <= '2012-06-30'.to_date and crianca.nascimento >= '2011-07-01'.to_date)
-                          crianca.grupo_id = 66
-                        else if(crianca.nascimento <= '2011-06-30'.to_date and crianca.nascimento >= '2010-07-01'.to_date)
-                               crianca.grupo_id = 77
-                             end
-                       end
-                 end
-             end
-         end
-      end
-     crianca.save
+ #def self.alteracao_classe
+ # @criancas_alteracao = Crianca.find( :all,:conditions => ["status = 'NA_DEMANDA'"])
+ #for crianca in @criancas_alteracao
+ # if  (crianca.nascimento <= '2015-10-31'.to_date and crianca.nascimento >= '2015-02-01'.to_date)
+ #     crianca.grupo_id  = 11
+ #    else if(crianca.nascimento <= '2015-01-31'.to_date and crianca.nascimento >= '2014-07-01'.to_date)
+ #          crianca.grupo_id = 22
+ #        else if(crianca.nascimento <= '2014-06-30'.to_date and crianca.nascimento >= '2013-07-01'.to_date)
+ #              crianca.grupo_id = 44
+ #            else if(crianca.nascimento <= '2013-06-30'.to_date and crianca.nascimento >= '2012-07-01'.to_date)
+ #                   crianca.grupo_id = 54
+ #                 else if(crianca.nascimento <= '2012-06-30'.to_date and crianca.nascimento >= '2011-07-01'.to_date)
+ #                         crianca.grupo_id = 66
+ #                       else if(crianca.nascimento <= '2011-06-30'.to_date and crianca.nascimento >= '2010-07-01'.to_date)
+ #                              crianca.grupo_id = 77
+ #                            end
+ #                      end
+ #                end
+ #            end
+ #        end
+ #     end
+ #    crianca.save#
 
-   end
-   end
+ #  end
+ #  end
 
 
    def self.nome_unidade(unidade)
