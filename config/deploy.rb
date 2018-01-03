48fcfa3ff81c5124622014a81d597fd24d0eb539
DesenvolvimentoNaor <informatica@seducpma.com>
2018-01-03 09:41:30 -0200

    03/01/2018

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,10 @@
-set :application, "192.168.0.12"
-#set :application, "192.168.0.21"
+#set :application, "192.168.0.12"
+set :application, "192.168.0.21"
 #set :application, "170.81.237.114"
 
 set :repository, "git://github.com/seducpma/demanda.git"
+set :user, "hera"
 #set :user, "servidor"
-#set :user, "hera"
-set :user, "servidor"
 set :use_sudo, false
 set :deploy_to, "/home/#{user}/demanda.seducpma.com"
 set :scm, :git
