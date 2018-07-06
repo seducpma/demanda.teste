f4d13598ba8d8f2d1c076fd1cb20d678d193c213
Your Name <you@example.com>
2018-07-06 12:49:28 -0300

    06-07-2018

--- app/controllers/alteracaos_controller.rb
+++ app/controllers/alteracaos_controller.rb
@@ -19,9 +19,9 @@ class AlteracaosController < ApplicationController
     @criancas_alteracao = Crianca.find( :all,:conditions => ["status = 'NA_DEMANDA'" ])
     for crianca in @criancas_alteracao
       anterior = crianca.grupo_id
-       if (crianca.nascimento < '2019-01-01'.to_date and crianca.nascimento >= '2017-06-01'.to_date)
+       if (crianca.nascimento < '2019-01-01'.to_date and crianca.nascimento >= '2017-02-01'.to_date)
            crianca.grupo_id = 1
-       else if(crianca.nascimento < '2017-06-01'.to_date and crianca.nascimento >= '2016-07-01'.to_date)
+       else if(crianca.nascimento < '2017-02-01'.to_date and crianca.nascimento >= '2016-07-01'.to_date)
                crianca.grupo_id = 2
             else if(crianca.nascimento < '2016-07-01'.to_date and crianca.nascimento >= '2016-01-01'.to_date)
                    crianca.grupo_id = 4
--- app/controllers/criancas_controller.rb
+++ app/controllers/criancas_controller.rb
@@ -112,9 +112,9 @@ end
 
 
     if (hoje > data)  and (data >= final)
-       if  (data <= Date.today.to_s and data >= '2017-06-01')
+       if  (data <= Date.today.to_s and data >= '2017-02-01')
        @crianca.grupo_id = 1
-        else if(data < '2017-06-01' and data >= '2016-07-01')
+        else if(data < '2017-02-01' and data >= '2016-07-01')
            @crianca.grupo_id = 2
            else if(data < '2016-07-01' and data >= '2016-01-01')
                   @crianca.grupo_id = 4
@@ -162,16 +162,14 @@ end
   # PUT /criancas/1
   # PUT /criancas/1.xml
   def update
-
-     #ALTERAR TAMBÈM AS DATAS NO CRIANCAS def aletraçoes controller  def alterar classes
-     @crianca = Crianca.find(params[:id])
+    @crianca = Crianca.find(params[:id])
 
     hoje = Date.today.to_s
     final = '2012-07-01'
     data=@crianca.nascimento.strftime("%Y-%m-%d")
-if  (data <= Date.today.to_s and data >= '2017-06-01')
+if  (data <= Date.today.to_s and data >= '2017-02-01')
        @crianca.grupo_id = 1
-        else if(data < '2017-06-01' and data >= '2016-07-01')
+        else if(data < '2017-02-01' and data >= '2016-07-01')
            @crianca.grupo_id = 2
            else if(data < '2016-07-01' and data >= '2016-01-01')
                   @crianca.grupo_id = 4
